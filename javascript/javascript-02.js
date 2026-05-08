d026c3f4ff48dafa532781624366b507dbd51db5
Luis.Aguilar <aguilar.luiseder27@gmail.com>
2026-05-08 16:53:21 -0600

    calculadora con parametros

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,50 +1,38 @@
 let botones = document.getElementsByClassName("numeros");
 let resultado = document.getElementById("resultado");
-
-let prm = "";
-let operacion = "";
+let prms = [];
+let operacion;
 
 for (const key in botones) {
   if (Object.prototype.hasOwnProperty.call(botones, key)) {
     const boton = botones[key];
+    // console.log(boton)
     boton.addEventListener("click", pintar);
   }
 }
 
 function pintar(e) {
+  // console.log(e.target.className)
   resultado.value += e.target.innerText;
 }
 
 let operadores = document.getElementsByClassName("operadores");
+console.log(operadores);
 
 for (const key in operadores) {
   if (Object.prototype.hasOwnProperty.call(operadores, key)) {
     const boton = operadores[key];
-
-    if (boton.innerText == "CE") {
-      boton.addEventListener("click", borrar);
-    }
-
-    if (boton.innerText == "+") {
-      boton.addEventListener("click", sumar);
-    }
-
-    if (boton.innerText == "-") {
-      boton.addEventListener("click", restar);
-    }
-
-    if (boton.innerText == "*") {
-      boton.addEventListener("click", multiplicar);
-    }
-
-    if (boton.innerText == "/") {
-      boton.addEventListener("click", dividir);
-    }
-
-    if (boton.innerText == "=") {
-      boton.addEventListener("click", igual);
-    }
-
+    console.log(boton);
+    if (boton.innerText == "Ce") boton.addEventListener("click", borrar);
+    if (boton.innerText == "+")
+      boton.addEventListener("click", obtener_parametros);
+    if (boton.innerText == "-")
+      boton.addEventListener("click", obtener_parametros);
+    if (boton.innerText == "*")
+      boton.addEventListener("click", obtener_parametros);
+    if (boton.innerText == "/")
+      boton.addEventListener("click", obtener_parametros);
+    if (boton.innerText == "=") boton.addEventListener("click", igual);
     boton.addEventListener("click", pintar2);
   }
 }
@@ -55,54 +43,48 @@ function pintar2(e) {
 
 function borrar() {
   resultado.value = "";
-  prm = "";
-  operacion = "";
 }
 
-function sumar(e) {
-  prm = resultado.value;
-  operacion = "+";
+function obtener_parametros(e) {
+  prms.push(resultado.value);
+  operacion = e.target.innerText;
   resultado.value = "";
-  e.target.style.backgroundColor = "red";
-}
-
-function restar(e) {
-  prm = resultado.value;
-  operacion = "-";
-  resultado.value = "";
-  e.target.style.backgroundColor = "red";
-}
-
-function multiplicar(e) {
-  prm = resultado.value;
-  operacion = "*";
-  resultado.value = "";
-  e.target.style.backgroundColor = "red";
-}
-
-function dividir(e) {
-  prm = resultado.value;
-  operacion = "/";
-  resultado.value = "";
-  e.target.style.backgroundColor = "red";
+  //e.target.style.backgroundColor = "red"
 }
 
 function igual() {
-  let prm2 = resultado.value;
-
-  if (operacion == "+") {
-    resultado.value = parseInt(prm) + parseInt(prm2);
-  }
-
-  if (operacion == "-") {
-    resultado.value = parseInt(prm) - parseInt(prm2);
-  }
-
-  if (operacion == "*") {
-    resultado.value = parseInt(prm) * parseInt(prm2);
-  }
-
-  if (operacion == "/") {
-    resultado.value = parseInt(prm) / parseInt(prm2);
+  prms.push(resultado.value);
+  let r = 0;
+  let bandera = false;
+  let op = operacion;
+  for (const key in prms) {
+    if (Object.prototype.hasOwnProperty.call(prms, key)) {
+      const prm = prms[key];
+      switch (op) {
+        case "+":
+          r += parseInt(prm);
+          break;
+        case "-":
+          if (bandera == false) {
+            r = parseInt(prm);
+            bandera = true;
+          } else r -= parseInt(prm);
+          break;
+        case "*":
+          if (bandera == false) {
+            r = parseInt(prm);
+            bandera = true;
+          } else r *= parseInt(prm);
+          break;
+        case "/":
+          if (bandera == false) {
+            r = parseInt(prm);
+            bandera = true;
+          } else r /= parseInt(prm);
+          break;
+      }
+    }
+    resultado.value = r;
+    prms = [];
   }
 }
